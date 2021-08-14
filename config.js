4c80ce7ec2704d442cb3e8ddaf1c529fc43d8525
kingdompro2 <>
2021-08-14 01:17:31 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+exports.TOKEN = 'ODc1OTA3NzU1ODI1ODQ4Mzcw.YRcWqg.IESrwnXWDULvCnPyM1O0d3UzMqg';
+
+exports.PREFIX = '?';
+
+exports.news_API = '3ca01c941e3d45869c2ff2051e5c51fe';
+
+exports.giphy_API = '33aDKdZWJLYu9NmbhfzHCVNR24d1H0Nx';
+
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
+
+exports.GENIUS_API_KEY = 'OMA5NkVcInO_V7an8657D94wR-xf_m7BsoLR_FPQLmJFfJiRNLShC9deauEElCbS';
